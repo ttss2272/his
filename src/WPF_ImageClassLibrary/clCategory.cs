d5b6f928647cbcf8b3c184f19c75c0f6f00408e5
sameer-teratech1 <sameer@teratech-solutions.com>
2015-11-30 19:41:17 +0530

    Sameer Add edit update and delete CategoryEntery

--- src/WPF_ImageClassLibrary/clCategory.cs
+++ src/WPF_ImageClassLibrary/clCategory.cs
@@ -14,7 +14,7 @@ namespace WPF_ImageClassLibrary
 
         public string saveCategory(int CatID, string CategoryName, int IsActive, int IsDeleted)
         {
-            int Result;
+            string Result;
             con = conn.getConnection();
             SqlCommand cmd = new SqlCommand("saveCategory_SP", con);
             cmd.CommandType = CommandType.StoredProcedure;
@@ -23,11 +23,11 @@ namespace WPF_ImageClassLibrary
             cmd.Parameters.AddWithValue("@IsActive", IsActive);
             cmd.Parameters.AddWithValue("@IsDeleted", IsDeleted);
             con.Open();
-            Result = cmd.ExecuteNonQuery();
+            Result = cmd.ExecuteScalar().ToString();
             con.Close();
-            if (Result == 1)
+            if ((Result == "Category Save Sucessfully!!!") || (Result == "Category Update Sucessfully!!!"))
             {
-                return "Category Save Successfully...!";
+                return Result;
             }
             else
             {
@@ -45,6 +45,60 @@ namespace WPF_ImageClassLibrary
             cmd.CommandType = CommandType.StoredProcedure;
             SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            con.Close();
+            return ds;
+        }
+
+        public DataSet BindCategory(int CatID, string Name)
+        {
+            con = conn.getConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("BindCategory_SP", con);
+            cmd.Parameters.AddWithValue("@CategoryID", CatID);
+            cmd.Parameters.AddWithValue("@CategoryName", Name);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            con.Close();
+            return ds;
+        }
+
+        public string DeleteCategory(int CatId, string UpdatedDate)
+        {
+            string Result;
+            con = conn.getConnection();
+            SqlCommand cmd = new SqlCommand("DeleteCategory_SP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@CategoryID", CatId);
+            cmd.Parameters.AddWithValue("@UpdatedDate", UpdatedDate);
+            con.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            if (Result == "Category Deleted Sucessfully!!!")
+            {
+                return Result;
+            }
+            else
+            {
+                return "Error To Delete";
+            }
+           
+        }
+
+        public DataSet BindCategoryName()
+        {
+            con = conn.getConnection();
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("BindCategoryName_SP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
             sqlDa.Fill(ds);
             con.Close();
             return ds;
---
+++ src/WPF_ImageClassLibrary/clSubCategory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WPF_ImageClassLibrary
+{
+  public  class clSubCategory
+    {
+        SqlConnection con = new SqlConnection();
+        clDatabase conn = new clDatabase();
+        public string SaveSubCategory(int SubCatId, int CategoryID,string SubCategoryName, int IsActive, int IsDeleted)
+        {
+            string Result;
+            con = conn.getConnection();
+            SqlCommand cmd = new SqlCommand("SaveSubCategory_SP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@SubCatID", SubCatId);
+            cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+            cmd.Parameters.AddWithValue("@SubCategoryName", SubCategoryName);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+            cmd.Parameters.AddWithValue("@IsDeleted", IsDeleted);
+            con.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            con.Close();
+            if ((Result == " Sub Category Save Sucessfully!!!") || (Result == " Sub Category Update Sucessfully!!!"))
+            {
+                return Result;
+            }
+            else
+            {
+                return "Error To Save";
+            }
+        }
+    }
+}
